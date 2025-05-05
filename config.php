f989079d37aa997c8e8b8a2c8c57b17307c18ae7
Pratik Pardeshi <89245723+Pratik-Pardeshi@users.noreply.github.com>
2025-05-05 12:37:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'your-rds-endpoint.amazonaws.com');
+define('DB_HOST', 'database-1.cpe8sey6kwca.ap-south-1.rds.amazonaws.com');
 define('DB_NAME', 'ecommerce');
 define('DB_USER', 'admin');
-define('DB_PASS', 'yourpassword');
+define('DB_PASS', 'admin123');
 ?>
